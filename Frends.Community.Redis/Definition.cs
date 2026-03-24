29da67701370aa4587a55223e487c62daa5fa573
Kalle Ljung <kalle@wearevoice.se>
2026-03-24 11:07:38 +0100

    Add RedisJSON & RediSearch support, drop legacy targets
    Breaking: Now targets .NET 8 only. Added support for RedisJSON (add/get/remove/merge JSON) and RediSearch (query, create/delete/update indexes). Introduced new tasks: Merge, Query, CreateIndex, DeleteIndex, UpdateIndex, CreateOrUpdateIndex. Updated dependencies (Newtonsoft.Json 13.0.4, StackExchange.Redis 2.12.4, NRedisStack 1.3.0). Extended input/output types for JSON and index operations. Switched tests to MSTest, added comprehensive coverage for new features. Updated documentation and metadata for all changes. Internal APIs exposed to tests.

--- Frends.Community.Redis/Definition.cs
+++ Frends.Community.Redis/Definition.cs
@@ -12,17 +12,31 @@ namespace Frends.Community.Redis
     /// <summary>
     /// Input-object
     /// </summary>
-    public enum ObjectType { KeyValuePair, Set }
+    public enum ObjectType { KeyValuePair, Set, Json }
 
     /// <summary>
     /// When value exists
     /// </summary>
     public enum WhenValueExist { InsertAlways = 0, InsertOnlyIfValueExists = 1, InsertOnlyIfValueDoesNotExist = 2 };
 
-    /// <summary>
-    /// Insert input-object 
-    /// </summary>
-    public class AddInput
+	/// <summary>
+	/// Index field definition type
+	/// </summary>
+	public enum FieldDefinitionType
+	{
+		Text,
+		Geo,
+		GeoShape,
+		Numeric,
+		Tag,
+		Vector
+	}
+
+
+	/// <summary>
+	/// Insert input-object 
+	/// </summary>
+	public class AddInput
     {
         /// <summary>
         /// Type of inserted object
@@ -41,7 +55,13 @@ namespace Frends.Community.Redis
         /// </summary>
         [UIHint(nameof(InputObjectType), "", ObjectType.Set)]
         public SetInput[] SetInput { get; set; }
-    }
+
+		/// <summary>
+		/// Json to insert
+		/// </summary>
+		[UIHint(nameof(InputObjectType), "", ObjectType.Json)]
+		public JsonInput[] JsonInput { get; set; }
+	}
 
     /// <summary>
     /// Get values input
@@ -67,13 +87,82 @@ namespace Frends.Community.Redis
         [DisplayFormat(DataFormatString = "Text")]
         [UIHint(nameof(ObjectType), "", ObjectType.Set)]
         public object SetKey { get; set; }
-    }
 
+		/// <summary>
+		/// Json key
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Text")]
+		[UIHint(nameof(ObjectType), "", ObjectType.Json)]
+		public object JsonKey { get; set; }
+	}
 
-    /// <summary>
-    /// Connection options
-    /// </summary>
-    public class Connection
+	/// <summary>
+	/// Contains input elements for the command task
+	/// </summary>
+	public class CommandInput
+	{
+		/// <summary>
+		/// The command to be executed.
+		/// Ex: idx:users
+		/// </summary>
+		public string Command { get; set; }
+
+		/// <summary>
+		/// Array of parameters for the command.
+		/// Ex: user:
+		/// </summary>
+		public object[] Parameters { get; set; }
+	}
+
+	/// <summary>
+	/// Merge input object for Redis JSON. 
+	/// Contains the JSON to merge and the path where the JSON should be merged. 
+	/// If path is not provided, it will be merged at root ($)
+	/// </summary>
+	public class MergeInput
+	{
+		/// <summary>
+		/// Object type
+		/// </summary>
+		[DefaultValue(ObjectType.KeyValuePair)]
+		public ObjectType ObjectType { get; set; }
+
+		/// <summary>
+		/// Json to merge
+		/// </summary>
+		[UIHint(nameof(ObjectType), "", ObjectType.Json)]
+		public JsonInput JsonInput { get; set; }
+	}
+
+	public class DeleteInput
+	{
+		/// <summary>
+		/// Object type
+		/// </summary>
+		[DefaultValue(ObjectType.KeyValuePair)]
+		public ObjectType ObjectType { get; set; }
+
+		/// <summary>
+		/// Set key and set values to delete
+		/// </summary>
+		[UIHint(nameof(ObjectType), "", ObjectType.Set)]
+		public SetInput SetInput { get; set; }
+
+		/// <summary>
+		/// Keys for Key-Value pairs to delete
+		/// </summary>
+		[UIHint(nameof(ObjectType), "", ObjectType.KeyValuePair)]
+		[DisplayFormat(DataFormatString = "Text")]
+		public object[] Key { get; set; }
+
+		[UIHint(nameof(ObjectType), "", ObjectType.Json)]
+		public JsonInput JsonInput { get; set; }
+	}
+
+	/// <summary>
+	/// Connection options
+	/// </summary>
+	public class Connection
     {
         /// <summary>
         /// Connection string
@@ -112,7 +201,7 @@ namespace Frends.Community.Redis
         /// </summary>
         [DefaultValue(1)]
         public int InputOutputCompletionPorts { get; set; }
-    }
+	}
 
     /// <summary>
     /// Input object for key-value pairs
@@ -174,48 +263,128 @@ namespace Frends.Community.Redis
         public object[] Value { get; set; }
     }
 
-    public class DeleteInput
-    {
-        /// <summary>
-        /// Object type
-        /// </summary>
-        [DefaultValue(ObjectType.KeyValuePair)]
-        public ObjectType ObjectType { get; set; }
+	public class JsonInput
+	{
+		/// <summary>
+		/// Json key
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Text")]
+		public object Key { get; set; }
 
-        /// <summary>
-        /// Set key and set values to delete
-        /// </summary>
-        [UIHint(nameof(ObjectType), "", ObjectType.Set)]
-        public SetInput SetInput { get; set; }
+		/// <summary>
+		/// Json value
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Text")]
+		public object Value { get; set; }
 
-        /// <summary>
-        /// Keys for Key-Value pairs to delete
-        /// </summary>
-        [UIHint(nameof(ObjectType), "", ObjectType.KeyValuePair)]
-        [DisplayFormat(DataFormatString = "Text")]
-        public object[] Key { get; set; }
-    }
+		/// <summary>
+		/// JSONPath to specify. Default is root $. Nonexisting paths are ignored.
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Text")]
+		public object Path { get; set; }
+	}
 
-    /// <summary>
-    /// Contains input elements for the command task
-    /// </summary>
-    public class CommandInput
-    {
-        /// <summary>
-        /// The command to be executed
-        /// </summary>
-        public string Command { get; set; }
+	public class JsonQueryInput
+	{
+		/// <summary>
+		/// Index name for Json query
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Text")]
+		public string IndexName { get; set; }
 
-        /// <summary>
-        /// Array of parameters for the command
-        /// </summary>
-        public object[] Parameters { get; set; }
-    }
+		/// <summary>
+		/// Json query string
+		/// Ex: Paul @age:[30 40]
+		/// </summary>
+		[DisplayFormat(DataFormatString = "Text")]
+		public string Query { get; set; }
+	}
 
-    /// <summary>
-    /// Output-object
-    /// </summary>
-    public class Result
+	public class IndexFieldDefinition
+	{
+		/// <summary>
+		/// Field definition type
+		/// </summary>
+		public FieldDefinitionType FieldType { get; set; }
+
+		/// <summary>
+		/// The identifier is a JSON Path expression
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Alias for th JSONPath expression 
+		/// </summary>
+		public string Attribute { get; set; }
+	}
+
+	public class IndexCreateInput
+	{
+		/// <summary>
+		/// The name of the index.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// The prefix of the keys that should be indexed.
+		/// </summary>
+		public string Prefix { get; set; }
+
+		/// <summary>
+		/// Array of fields for the index.
+		/// </summary>
+		public IndexFieldDefinition[] Parameters { get; set; }
+	}
+
+	public class IndexDeleteInput
+	{
+		/// <summary>
+		/// The name of the index.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		///  Delete the documents associated with the index.
+		/// </summary>
+		[DefaultValue(false)]
+		public bool DeleteDocuments { get; set; }
+	}
+
+	public class IndexUpdateInput
+	{
+		/// <summary>
+		/// The name of the index.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Array of fields for the index.
+		/// </summary>
+		public IndexFieldDefinition[] Parameters { get; set; }
+	}
+
+	public class IndexCreateOrUpdateInput
+	{
+		/// <summary>
+		/// The name of the index.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// The prefix of the keys that should be indexed.
+		/// </summary>
+		public string Prefix { get; set; }
+
+		/// <summary>
+		/// Array of fields for the index.
+		/// </summary>
+		public IndexFieldDefinition[] Parameters { get; set; }
+	}
+
+	/// <summary>
+	/// Output-object
+	/// </summary>
+	public class Result
     {
         private readonly Lazy<JToken> _jToken;
 
